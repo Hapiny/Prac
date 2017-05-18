952c901f230a05f812219ed3dae1e1eda7c0c097
Hapiny <tmironov6@gmail.com>
2017-05-18 12:47:00 +0300

    Added User.h

---
+++ include/User.h
@@ -0,0 +1,16 @@
+#include <string>
+
+class User 
+{
+public:
+	User(std::string name, std::string nickname, std::string password, int id);
+	std::string get_name() const;
+	std::string get_nickname() const;
+	std::string get_password() const;
+	int get_id() const;
+private:
+	std::string name;
+	std::string nickname;
+	std::string password;
+	int id;
+}
