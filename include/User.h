fd9c4f631ac358e2602c50b060e3093f6397f18f
Hapiny <tmironov6@gmail.com>
2017-05-18 12:50:39 +0300

    Convert indentation to spaces in User.h

--- include/User.h
+++ include/User.h
@@ -3,14 +3,14 @@
 class User 
 {
 public:
-	User(std::string name, std::string nickname, std::string password, int id);
-	std::string get_name() const;
-	std::string get_nickname() const;
-	std::string get_password() const;
-	int get_id() const;
+    User(std::string name, std::string nickname, std::string password, int id);
+    std::string get_name() const;
+    std::string get_nickname() const;
+    std::string get_password() const;
+    int get_id() const;
 private:
-	std::string name;
-	std::string nickname;
-	std::string password;
-	int id;
+    std::string name;
+    std::string nickname;
+    std::string password;
+    int id;
 }
